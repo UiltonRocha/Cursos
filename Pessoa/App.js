7a4d57576a007ecaa122a983b8e744f4aa053711
Uilton Rocha Novaes <unovaes@scale.com.br>
2018-10-31 14:30:29 -0200

    Listando nomes de pessoas da API

--- Pessoa/App.js
+++ Pessoa/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import {StyleSheet, Text, View} from 'react-native';
+import axios from 'axios';
 import Header from './src/components/Header';
 
 export default class App extends React.Component {
-  render() {
-    return (
+  constructor(props){
+    super(props);
+    this.state ={
+      peoples: []
+    };
+  }
+
+  componentDidMount(){
+    axios
+    .get('https://randomuser.me/api/?nat=br&results=5')
+    .then(response => {
+      const {results} = response.data;
+
+      this.setState({
+        peoples: results
+      });
+    })
+  }
+
+
+  renderList(){
+    const textElements = this.state.peoples.map(people => {
+      const {first} = people.name;
+      return <Text key={first}>{first}</Text>
+    });
+    return textElements;
+  }
+  render(){
+    return(
       <View>
         <Header title="Pessoas!" />
+        {this.renderList()}
       </View>
     );
   }
